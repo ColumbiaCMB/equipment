8acdb941dc92bf957473db72062cacea8e6e65a2
Daniel Flanigan <daniel.isaiah.flanigan@gmail.com>
2015-01-13 19:19:54 -0500

    updated animate.py

--- Lake_Shore/animate.py
+++ Lake_Shore/animate.py
@@ -8,33 +8,39 @@ from equipment.Lake_Shore import gaussmeter
 gm = gaussmeter.Gaussmeter425('/dev/tty.SLAB_USBtoUART')
 
 length = 100
-data = deque(np.zeros(length), maxlen=length)
-t0 = time.time()
-t = deque(np.zeros(length), maxlen=length)
+data = deque([0], maxlen=length)
+times = deque([0], maxlen=length)
 fig, ax = plt.subplots(figsize=(3, 3))
-ax.set_ylim(-1, 1)
-(line,) = ax.plot(t, data, '-')
+ax.set_xlabel('time [s]')
+ax.set_ylabel('field [{}]'.format(gm.field_units))
+(line,) = ax.plot(times, data, '-r')
 
-def append_gaussmeter_data():
+def get_gaussmeter():
+    t0 = time.time()
     while True:
-        t.append(time.time() - t0)
-        data.append(gm.field)
-        yield (t, data)
+        t = time.time() - t0
+        f = gm.field
+        yield t, f
 
-def append_random_data():
+def get_random():
+    t0 = time.time()
     while True:
-        t.append(time.time()) - t0
-        data.append(np.random.randn())
-        yield (t, data)
+        t = time.time() - t0
+        r = np.random.randn()
+        yield t, r
 
-def animate((t, data)):
-    padding = 0.1 * (max(data) - min(data))
+def animate(framedata):
+    t, y = framedata
+    times.append(t)
+    data.append(y)
+    ax.set_xlim(min(times), max(times))
+    padding = 0.1 * (max(data) - min(data)) or 1
     ax.set_ylim(min(data) - padding, max(data) + padding)
-    ax.set_xlim(min(t), max(t))
-    line.set_xdata(t)
+    line.set_xdata(times)
     line.set_ydata(data)
-    return (ax, line)
+    return ax, line
 
-anim = animation.FuncAnimation(fig, animate, frames=append_gaussmeter_data, interval=10)
+anim = animation.FuncAnimation(fig, animate, get_gaussmeter, interval=1)
+#anim = animation.FuncAnimation(fig, animate, frames=get_random, interval=100)
 
 plt.show()
