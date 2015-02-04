8ad105e8ca536a1d151a896402037b12bc674937
Daniel Flanigan <daniel.isaiah.flanigan@gmail.com>
2015-02-04 14:23:01 -0500

    zbd.py

--- VDI/zbd.py
+++ VDI/zbd.py
@@ -6,6 +6,9 @@ class ZBD(object):
 
     xlsx_filename = '13067-columbia-wr6.5zbd-datapoints.xlsx'
 
+    # This is -15 dBm, or about 32 uW, which is the maximum power that keeps the output compression under 0.5 dB.
+    maximum_linear_power = 1e-3 * 10**(-15 / 10)
+
     def __init__(self):
         data = pd.ExcelFile(os.path.join(os.path.dirname(__file__), self.xlsx_filename)).parse('Sheet1')
         self.frequency_data_Hz = 1e9 * np.array(data['Freq (GHz)'])
@@ -13,3 +16,6 @@ class ZBD(object):
 
     def responsivity(self, frequency):
         return np.interp(frequency, self.frequency_data_Hz, self.responsivity_data_V_per_W)
+
+    def maximum_linear_voltage(self, frequency):
+        return self.maximum_linear_power * self.responsivity(frequency)
